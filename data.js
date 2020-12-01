be6f583fc017beb0951f1fb6276f750342f61e5d
flamesdev <johannpayer123@gmail.com>
2020-12-01 13:45:56 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,105 @@
+/* eslint-disable no-var */
+/* eslint-disable no-unused-vars */
+const usaColors = [
+  'ad001e',
+  'f5f5f5',
+  'ffffff',
+  '2578B2',
+];
+var holidays = [
+  [ 'new years', 1, 1, null, [
+    '03172f',
+    '062844',
+    '093657',
+    'fff7b6',
+    'efd466',
+    'd7b030',
+  ] ],
+  [ `valentine's day`, 2, 14, null, [
+    'fa7ba8',
+    'f38fb3',
+    'f0a2bc',
+    'f3bed0',
+    'f8d8e3',
+    'fdf2f6',
+  ] ],
+  [ `saint patrick's day`, 3, 17, null, [
+    '254519',
+    '386b25',
+    '4c9231',
+    '5fba3d',
+    '7ccb5e',
+  ] ],
+  [ 'easter', null, null, (year) => {
+    let c = Math.floor(year / 100);
+    let n = year - 19 * Math.floor(year / 19);
+    let k = Math.floor((c - 17) / 25);
+    let i = c - Math.floor(c / 4) - Math.floor((c - k) / 3) + 19 * n + 15;
+    i = i - 30 * Math.floor((i / 30));
+    i = i - Math.floor(i / 28) * (1 - Math.floor(i / 28) * Math.floor(29 / (i + 1)) * Math.floor((21 - n) / 11));
+    let j = year + Math.floor(year/4) + i + 2 - c + Math.floor(c/4);
+    j = j - 7 * Math.floor(j / 7);
+    let l = i - j
+    let month = 3 + Math.floor((l + 40) / 44);
+
+    return [ month, l + 28 - 31 * Math.floor(month / 4) ];
+  }, [
+    '5374a0',
+    'a3c3e9',
+    'f1f0f4',
+    '9c6140',
+    'dc9c4d',
+    '789429',
+  ] ],
+  [ `mother's day`, null, null, (year) => {
+    const month = 5;
+    const day = (new Date(year, month - 1, 1)).getDay();
+    return [ month, 8 + (7 - day) ];
+  }, [
+    'f14472',
+    'f17db6',
+    'fed40c',
+    'b8c500',
+    '85af4b',
+  ] ],
+  [ 'the fourth of july', 6, 4, null, usaColors ],
+  [ `father's day`, null, null, (year) => {
+    const month = 6;
+    const day = (new Date(year, month - 1, 1)).getDay();
+    return [ month, 15 + (7 - day) ];
+  }, [
+    'Caac3e',
+    '4f4c47',
+    '7ac3bc',
+    'e6e7a3',
+    'eb7140',
+  ] ],
+  [ 'halloween', 10, 31, null, [
+    'e98f4e',
+    '3c4349',
+    'f5e9e2',
+    '836b5a',
+    '584137',
+  ] ],
+  [ 'veterans day', 11, 11, null, usaColors ],
+  [ 'thanksgiving', null, null, (year) => {
+    const month = 11;
+    const day = (new Date(year, month - 1, 30)).getDay();
+    return [ month, (day >= 4 ? 34 : 27) - day ];
+  }, [
+    '70362a',
+    'a17c22',
+    'debf7e',
+    'd5673b',
+    'be5634',
+    'b5ad15',
+  ] ],
+  [ 'christmas', 12, 25, null, [
+    'de6960',
+    'f4c87c',
+    '6da06e',
+    '39775a',
+    'f3f3eb',
+    'b18e73',
+  ] ],
+].map((x) => ({ name : x[0], month : x[1], day : x[2], dateFunc : x[3], colors : x[4] }));
---
+++ script.js
@@ -0,0 +1,190 @@
+let context;
+let particles;
+let nearestHoliday;
+let holidayIndex;
+
+let mouseX;
+let mouseY;
+let forceRender = true;
+
+function updateCanvasSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  context.globalAlpha = 0.76;
+}
+
+function updateParticles() {
+  let doRender = forceRender;
+  if (forceRender) {
+    forceRender = false;
+  }
+
+  if (!doRender) {
+    if (document.hasFocus()) {
+      particles.forEach((particle) => {
+        const xDiff = particle.x * canvas.width - mouseX;
+        const yDiff = particle.y * canvas.height - mouseY;
+        const distance = Math.sqrt((xDiff ** 2) + (yDiff ** 2));
+  
+        if (distance < 150) {
+          const multiple = 10 / (Math.PI * (particle.size ** 2));
+          particle.xVel += xDiff * multiple;
+          particle.yVel += yDiff * multiple;
+  
+          doRender = true;
+        }
+      });
+    }
+
+    if (!doRender) {
+      doRender = particles.some((particle) => [ 'x', 'y' ].some((x) => Math.abs(particle[x + 'Vel']) >= 0.01));
+    }
+  }
+
+  if (doRender) {
+    function getBoundValue(num) {
+      return Math.max(Math.min(num, 1), 0);
+    }
+
+    particles.forEach((particle) => {
+      if (particle.sizeScale !== 1) {
+        particle.sizeScale = Math.min(particle.sizeScale * 1.14, 1);
+      }
+
+      [ [ 'x', canvas.width ], [ 'y', canvas.height ] ].forEach(([ propName, dimension ]) => {
+        const velPropName = propName + 'Vel';
+        particle[propName] += particle[velPropName] / dimension;
+
+        particle[velPropName] *= 0.94;
+
+        const value = particle[propName];
+        if (value < 0 || value > 1) {
+          particle[propName] = getBoundValue(value);
+          particle[velPropName] *= -1;
+        }
+      });
+    });
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach((x) => {
+      context.fillStyle = x.color;
+      context.beginPath();
+      context.arc(x.x * canvas.width, x.y * canvas.height, x.size * x.sizeScale, 2 * Math.PI, false);
+      context.fill();
+    });
+  }
+
+  requestAnimationFrame(updateParticles);
+}
+
+function updateText() {
+  countdown.innerHTML = `${nearestHoliday.name}<br>${dayjs().to(nearestHoliday.date)}`;
+}
+
+function updateNearestHoliday(index) {
+  const year = (new Date()).getFullYear();
+  const now = Date.now();
+
+  function addDateProp(x) {
+    let date = dayjs([ year, x.month, x.dateFunc ? x.dateFunc(year) : x.day ]);
+    if (date.valueOf() < now) {
+      date = date.set('year', year + 1);
+    }
+
+    return { ...x, date };
+  }
+
+  nearestHoliday = index === null ? holidays.map(addDateProp).sort((x1, x2) => x1.date.valueOf() - x2.date.valueOf())[0] : addDateProp(holidays[index]);
+  holidayIndex = index ?? holidays.findIndex((x) => x.name === nearestHoliday.name);
+}
+
+function reset() {
+  function getRandomDirection() {
+    return (Math.random() * 2) - 1;
+  }
+
+  function getRandomCoordinate() {
+    return 0.5 + (getRandomDirection()) * 0.1;
+  }
+
+  function getRandomVelocity() {
+    return getRandomDirection() * 3;
+  }
+
+  particles = [];
+  for (let i = 0; i < 3000; i++) {
+    const { colors } = nearestHoliday;
+    particles.push({
+      x : getRandomCoordinate(),
+      y : getRandomCoordinate(),
+      xVel : getRandomVelocity(),
+      yVel : getRandomVelocity(),
+      size : 1 / (1 + (Math.E ** (getRandomDirection() * 3))) * 6 + 2,
+      sizeScale : 0.5,
+      color : '#' + colors[Math.floor(colors.length * Math.random())],
+    });
+  }
+
+  updateText();
+  forceRender = true;
+}
+
+window.addEventListener('load', () => {
+  dayjs.extend(dayjs_plugin_relativeTime);
+
+  const seperator = '?holiday=';
+  const param = decodeURI(window.location.href).split('&').find((x) => x.includes(seperator));
+  let index = null;
+  if (param) {
+    const temp = holidays.findIndex((x) => x.name === param.split(seperator)[1]);
+    if (temp !== -1) {
+      index = temp;
+    }
+  }
+
+  updateNearestHoliday(index);
+  updateText();
+
+  context = canvas.getContext('2d');
+  updateCanvasSize();
+
+  reset();
+  requestAnimationFrame(updateParticles);
+  setInterval(() => {
+    if (nearestHoliday.date.valueOf() < Date.now()) {
+      updateNearestHoliday(null);
+      reset();
+    }
+
+    updateText();
+  }, 1000);
+});
+
+window.addEventListener('resize', () => {
+  updateCanvasSize();
+  reset();
+});
+
+window.addEventListener('mousemove', (event) => {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+});
+
+window.addEventListener('keydown', (event) => {
+  function wrapIndex(index, length) {
+    return ((index % length) + length) % length;
+  }
+
+  const { key } = event;
+  let change;
+  if (key === 'ArrowLeft') {
+    change = -1;
+  } else if (key === 'ArrowRight') {
+    change = 1;
+  }
+
+  if (change) {
+    updateNearestHoliday(wrapIndex(holidayIndex + change, holidays.length));
+    reset();
+  }
+});
